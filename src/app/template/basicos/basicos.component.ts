dfb824df9bb476cadb5daba0c3d70858f3bee5bc
CoopNuevaVida-Manu <mvilleda@coopnuevavida.com>
2022-02-09 16:12:21 -0600

    Formularios template

---
+++ src/app/template/basicos/basicos.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styles: [
+  ]
+})
+export class BasicosComponent implements OnInit {
+
+  @ViewChild('miFormulario') miFormulario !: NgForm
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  nombreValido(): boolean{
+    return this.miFormulario?.form.controls["producto"]?.invalid &&
+           this.miFormulario?.form.controls["producto"]?.touched
+  }
+  precioValido(): boolean{
+    return this.miFormulario?.form.controls["precio"]?.touched &&
+           this.miFormulario?.form.controls["precio"]?.value < 0.001
+  }
+  guardar(){
+    //console.log(miFormulario.value)
+  }
+
+}
